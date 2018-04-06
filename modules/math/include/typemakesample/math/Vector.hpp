67b587d6b36969e58c2dac985cf3fc162aa919d0
rex <r_ex@163.com>
2018-04-06 23:10:25 +0800

    add sample project

---
+++ modules/math/include/typemakesample/math/Vector.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <array>
+#include <ostream>
+
+namespace typemakesample
+{
+namespace math
+{
+
+struct Vector3d
+{
+public:
+    std::array<double, 3> data;
+
+    static double dot(Vector3d left, Vector3d right);
+    static Vector3d cross(Vector3d left, Vector3d right);
+    bool operator ==(Vector3d right);
+    bool operator !=(Vector3d right);
+};
+
+std::ostream & operator <<(std::ostream & stream, const Vector3d & v);
+
+}
+}
