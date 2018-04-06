b188a16e855a76052bf4d72bc3706793f264bb3b
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2018-04-07 01:24:58 +0800

    add Visual Studio solution file support

---
+++ typemake/Src/Base/Hash.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TypeMake
+{
+    public class Hash
+    {
+        public static String GetHashForPath(String Path, int Length)
+        {
+            var Bytes = Encoding.UTF8.GetBytes(Path);
+            Byte[] Hash;
+            using (var sha256 = new SHA256Managed())
+            {
+                Hash = sha256.ComputeHash(Bytes);
+            }
+            return String.Join("", Hash.Select(b => b.ToString("X2"))).Substring(0, Length);
+        }
+    }
+}
---
+++ typemake/Src/Schema/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TypeMake
+{
+    public class ProjectReference
+    {
+        public String Id;
+        public String Name;
+        public String VirtualDir;
+        public String FilePath;
+    }
+}
