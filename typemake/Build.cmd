e76f81f8fc2c2413e344734a22cd67be66222e2e
rex <r_ex@163.com>
2018-11-13 02:46:37 +0800

    refactor build batch

--- typemake/Build.cmd
+++ typemake/Build.cmd
@@ -5,10 +5,16 @@ if "%SUB_NO_PAUSE_SYMBOL%"=="1" set NO_PAUSE_SYMBOL=1
 if /I "%COMSPEC%" == %CMDCMDLINE% set NO_PAUSE_SYMBOL=1
 set SUB_NO_PAUSE_SYMBOL=1
 
-PATH %ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;%PATH%
+for %%p in (Enterprise Professional Community BuildTools) do (
+  if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%p" (
+    set VSDir="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%p"
+  )
+)
+set VSDir=%VSDir:"=%
+echo VSDir=%VSDir%
 
 set EXIT_CODE=0
-MSBuild Src\TypeMake.sln /t:Rebuild /p:Configuration=Release
+"%VSDir%\MSBuild\15.0\Bin\MSBuild.exe" Src\TypeMake.sln /t:Rebuild /p:Configuration=Release
 set /A EXIT_CODE^|=%ERRORLEVEL%
 
 if not "%NO_PAUSE_SYMBOL%"=="1" pause
