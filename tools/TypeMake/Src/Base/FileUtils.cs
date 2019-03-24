c1a1d52faef81f81aa38327e79726ff7be6b869d
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2019-03-24 12:44:29 +0800

    add FileUtils for file copying

---
+++ tools/TypeMake/Src/Base/FileUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace TypeMake
+{
+    public static class FileUtils
+    {
+        public static void Copy(PathString SourceFilePath, PathString DestinationFilePath, bool CopyOnlyOnModified)
+        {
+            if (CopyOnlyOnModified)
+            {
+                if (File.Exists(DestinationFilePath))
+                {
+                    var NewContent = File.ReadAllBytes(SourceFilePath);
+                    var OriginalContent = File.ReadAllBytes(DestinationFilePath);
+                    if (NewContent.SequenceEqual(OriginalContent))
+                    {
+                        return;
+                    }
+                }
+            }
+            var Dir = DestinationFilePath.Parent;
+            if (!Directory.Exists(Dir))
+            {
+                Directory.CreateDirectory(Dir);
+            }
+            File.Copy(SourceFilePath, DestinationFilePath, true);
+        }
+    }
+}
