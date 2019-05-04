c49c71ea029958e9876915f8e2eb296e0b581c3d
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2019-05-04 11:36:22 +0800

    split RetypemakeScriptReader from Program

---
+++ tools/TypeMake/Src/Make/RetypemakeScriptReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TypeMake
+{
+    public static class RetypemakeScriptReader
+    {
+        public static void Read(PathString RetypemakeScriptPath)
+        {
+            String[] Lines;
+            Regex rVariable;
+            if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "cmd")
+            {
+                Lines = File.ReadAllLines(RetypemakeScriptPath, System.Text.Encoding.Default);
+                rVariable = new Regex(@"^set\s+(""(?<Key>[^=]+)=(?<Value>.*)""|(?<Key>[^=]+)=(?<Value>.*))\s*$");
+            }
+            else if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "sh")
+            {
+                Lines = File.ReadAllLines(RetypemakeScriptPath, new System.Text.UTF8Encoding(false));
+                rVariable = new Regex(@"^export\s+(?<Key>[^=]+)=('(?<Value>.*)'|(?<Value>.*))\s*$");
+            }
+            else
+            {
+                throw new InvalidOperationException("InvalidRetypemakeScript");
+            }
+            foreach (var Line in Lines)
+            {
+                var Match = rVariable.Match(Line);
+                if (Match.Success)
+                {
+                    var Key = Match.Result("${Key}");
+                    var Value = Match.Result("${Value}");
+                    if (Key == "BuildDirectory")
+                    {
+                        Environment.SetEnvironmentVariable(Key, RetypemakeScriptPath.FullPath.Parent);
+                    }
+                    else
+                    {
+                        Environment.SetEnvironmentVariable(Key, Value);
+                    }
+                }
+            }
+        }
+    }
+}
--- tools/TypeMake/Src/Program.cs
+++ tools/TypeMake/Src/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace TypeMake
 {
@@ -43,39 +41,7 @@ namespace TypeMake
             if (argv.Length == 1)
             {
                 var RetypemakeScriptPath = argv[0].AsPath();
-                String[] Lines;
-                Regex rVariable;
-                if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "cmd")
-                {
-                    Lines = File.ReadAllLines(RetypemakeScriptPath, System.Text.Encoding.Default);
-                    rVariable = new Regex(@"^set\s+(""(?<Key>[^=]+)=(?<Value>.*)""|(?<Key>[^=]+)=(?<Value>.*))\s*$");
-                }
-                else if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "sh")
-                {
-                    Lines = File.ReadAllLines(RetypemakeScriptPath, new System.Text.UTF8Encoding(false));
-                    rVariable = new Regex(@"^export\s+(?<Key>[^=]+)=('(?<Value>.*)'|(?<Value>.*))\s*$");
-                }
-                else
-                {
-                    throw new InvalidOperationException("InvalidRetypemakeScript");
-                }
-                foreach (var Line in Lines)
-                {
-                    var Match = rVariable.Match(Line);
-                    if (Match.Success)
-                    {
-                        var Key = Match.Result("${Key}");
-                        var Value = Match.Result("${Value}");
-                        if (Key == "BuildDirectory")
-                        {
-                            Environment.SetEnvironmentVariable(Key, RetypemakeScriptPath.FullPath.Parent);
-                        }
-                        else
-                        {
-                            Environment.SetEnvironmentVariable(Key, Value);
-                        }
-                    }
-                }
+                RetypemakeScriptReader.Read(RetypemakeScriptPath);
             }
             else if (argv.Length != 0)
             {
