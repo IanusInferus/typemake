b83ecd9d35ad4eaf7b1ff70e518cc758c6e567ac
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2019-05-04 11:32:08 +0800

    split Generation from Program

--- tools/TypeMake/Src/Make/Generation.cs
+++ tools/TypeMake/Src/Make/Generation.cs
@@ -1,100 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace TypeMake
 {
-    public partial class Program
+    public static class Generation
     {
-        public static int Main(String[] args)
-        {
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                return MainInner(args);
-            }
-            else
-            {
-                try
-                {
-                    return MainInner(args);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                    return 1;
-                }
-            }
-        }
-
-        public static int MainInner(String[] args)
-        {
-            var argv = args.Where(arg => !arg.StartsWith("--") && !arg.Contains("=")).ToArray();
-            var options = args.Where(arg => arg.StartsWith("--")).Select(arg => arg.Substring(2).Split(new Char[] { ':' }, 2)).GroupBy(p => p[0]).ToDictionary(g => g.Key, g => g.Last().Skip(1).SingleOrDefault(), StringComparer.OrdinalIgnoreCase);
-            var optionLists = args.Where(arg => arg.StartsWith("--")).Select(arg => arg.Substring(2).Split(new Char[] { ':' }, 2)).GroupBy(p => p[0]).ToDictionary(g => g.Key, g => g.Select(Value => Value.Skip(1).SingleOrDefault()).ToList(), StringComparer.OrdinalIgnoreCase);
-
-            var Help = options.ContainsKey("help");
-            if (Help)
-            {
-                DisplayInfo();
-                return 0;
-            }
-            if (argv.Length == 1)
-            {
-                var RetypemakeScriptPath = argv[0].AsPath();
-                String[] Lines;
-                Regex rVariable;
-                if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "cmd")
-                {
-                    Lines = File.ReadAllLines(RetypemakeScriptPath, System.Text.Encoding.Default);
-                    rVariable = new Regex(@"^set\s+(""(?<Key>[^=]+)=(?<Value>.*)""|(?<Key>[^=]+)=(?<Value>.*))\s*$");
-                }
-                else if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "sh")
-                {
-                    Lines = File.ReadAllLines(RetypemakeScriptPath, new System.Text.UTF8Encoding(false));
-                    rVariable = new Regex(@"^export\s+(?<Key>[^=]+)=('(?<Value>.*)'|(?<Value>.*))\s*$");
-                }
-                else
-                {
-                    throw new InvalidOperationException("InvalidRetypemakeScript");
-                }
-                foreach (var Line in Lines)
-                {
-                    var Match = rVariable.Match(Line);
-                    if (Match.Success)
-                    {
-                        var Key = Match.Result("${Key}");
-                        var Value = Match.Result("${Value}");
-                        if (Key == "BuildDirectory")
-                        {
-                            Environment.SetEnvironmentVariable(Key, RetypemakeScriptPath.FullPath.Parent);
-                        }
-                        else
-                        {
-                            Environment.SetEnvironmentVariable(Key, Value);
-                        }
-                    }
-                }
-            }
-            else if (argv.Length != 0)
-            {
-                DisplayInfo();
-                return 1;
-            }
-
-            foreach (var p in args.Where(arg => arg.Contains("=")).Select(arg => arg.Split('=')))
-            {
-                Environment.SetEnvironmentVariable(p[0], p[1]);
-            }
-
-            var Quiet = options.ContainsKey("quiet");
-            Generate(Quiet);
-            Console.WriteLine("TypeMake successful.");
-            return 0;
-        }
-
-        private static void Generate(bool Quiet)
+        public static void Run(bool Quiet)
         {
             var Memory = new Shell.EnvironmentVariableMemory();
             var VariablesAndVariableItems = VariableCollection.GetVariableItems();
@@ -258,21 +170,11 @@ namespace TypeMake
             }
         }
 
-        public static void WriteLineError(String Line)
+        private static void WriteLineError(String Line)
         {
             Shell.SetForegroundColor(ConsoleColor.Red);
             Console.Error.WriteLine(Line);
             Shell.ResetColor();
-        }
-
-        public static void DisplayInfo()
-        {
-            Console.WriteLine(@"TypeMake");
-            Console.WriteLine(@"Usage:");
-            Console.WriteLine(@"TypeMake [<RetypemakeScript>] <Variable>* [--quiet] [--help]");
-            Console.WriteLine(@"RetypemakeScript batch or bash file to get environment variables for diagnostics");
-            Console.WriteLine(@"Variable <Key>=<Value> additional environment variables that only take effect in the call");
-            Console.WriteLine(@"--quiet no interactive variable input, all variables must be input from environment variables");
         }
     }
 }
---
+++ tools/TypeMake/Src/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TypeMake
+{
+    public partial class Program
+    {
+        public static int Main(String[] args)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                return MainInner(args);
+            }
+            else
+            {
+                try
+                {
+                    return MainInner(args);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return 1;
+                }
+            }
+        }
+
+        public static int MainInner(String[] args)
+        {
+            var argv = args.Where(arg => !arg.StartsWith("--") && !arg.Contains("=")).ToArray();
+            var options = args.Where(arg => arg.StartsWith("--")).Select(arg => arg.Substring(2).Split(new Char[] { ':' }, 2)).GroupBy(p => p[0]).ToDictionary(g => g.Key, g => g.Last().Skip(1).SingleOrDefault(), StringComparer.OrdinalIgnoreCase);
+            var optionLists = args.Where(arg => arg.StartsWith("--")).Select(arg => arg.Substring(2).Split(new Char[] { ':' }, 2)).GroupBy(p => p[0]).ToDictionary(g => g.Key, g => g.Select(Value => Value.Skip(1).SingleOrDefault()).ToList(), StringComparer.OrdinalIgnoreCase);
+
+            var Help = options.ContainsKey("help");
+            if (Help)
+            {
+                DisplayInfo();
+                return 0;
+            }
+            if (argv.Length == 1)
+            {
+                var RetypemakeScriptPath = argv[0].AsPath();
+                String[] Lines;
+                Regex rVariable;
+                if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "cmd")
+                {
+                    Lines = File.ReadAllLines(RetypemakeScriptPath, System.Text.Encoding.Default);
+                    rVariable = new Regex(@"^set\s+(""(?<Key>[^=]+)=(?<Value>.*)""|(?<Key>[^=]+)=(?<Value>.*))\s*$");
+                }
+                else if (RetypemakeScriptPath.Extension.ToLowerInvariant() == "sh")
+                {
+                    Lines = File.ReadAllLines(RetypemakeScriptPath, new System.Text.UTF8Encoding(false));
+                    rVariable = new Regex(@"^export\s+(?<Key>[^=]+)=('(?<Value>.*)'|(?<Value>.*))\s*$");
+                }
+                else
+                {
+                    throw new InvalidOperationException("InvalidRetypemakeScript");
+                }
+                foreach (var Line in Lines)
+                {
+                    var Match = rVariable.Match(Line);
+                    if (Match.Success)
+                    {
+                        var Key = Match.Result("${Key}");
+                        var Value = Match.Result("${Value}");
+                        if (Key == "BuildDirectory")
+                        {
+                            Environment.SetEnvironmentVariable(Key, RetypemakeScriptPath.FullPath.Parent);
+                        }
+                        else
+                        {
+                            Environment.SetEnvironmentVariable(Key, Value);
+                        }
+                    }
+                }
+            }
+            else if (argv.Length != 0)
+            {
+                DisplayInfo();
+                return 1;
+            }
+
+            foreach (var p in args.Where(arg => arg.Contains("=")).Select(arg => arg.Split('=')))
+            {
+                Environment.SetEnvironmentVariable(p[0], p[1]);
+            }
+
+            var Quiet = options.ContainsKey("quiet");
+            Generation.Run(Quiet);
+            Console.WriteLine("TypeMake successful.");
+            return 0;
+        }
+
+        public static void DisplayInfo()
+        {
+            Console.WriteLine(@"TypeMake");
+            Console.WriteLine(@"Usage:");
+            Console.WriteLine(@"TypeMake [<RetypemakeScript>] <Variable>* [--quiet] [--help]");
+            Console.WriteLine(@"RetypemakeScript batch or bash file to get environment variables for diagnostics");
+            Console.WriteLine(@"Variable <Key>=<Value> additional environment variables that only take effect in the call");
+            Console.WriteLine(@"--quiet no interactive variable input, all variables must be input from environment variables");
+        }
+    }
+}
