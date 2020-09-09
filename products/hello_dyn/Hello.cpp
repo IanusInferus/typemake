027e3564e47639f6c845d4e54115adfe40378af2
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2020-09-09 20:27:36 +0800

    fix printf text

--- products/hello_dyn/Hello.cpp
+++ products/hello_dyn/Hello.cpp
@@ -9,7 +9,7 @@ int main(int argc, char ** argv)
     typemakesample_Vector3d__ctor(1, 0, 0, &a);
     typemakesample_Vector3d__ctor(1, 1, 0, &b);
     auto result = typemakesample_Vector3d_dot(a, b);
-    std::printf("Vector3d::cross({{{1, 0, 0}}}, {{{0, 1, 0}}}) = %f\n", result);
+    std::printf("Vector3d::dot({{{1, 0, 0}}}, {{{0, 1, 0}}}) = %f\n", result);
     typemakesample_Vector3d__dtor(a);
     typemakesample_Vector3d__dtor(b);
 
