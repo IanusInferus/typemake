6d291ab4a926c99260c7cfaf0f562cc9d10f1a59
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2021-10-08 19:39:33 +0800

    fix file path sorting on MacOS

--- build-tools/TypeMake/Src/Base/FileSystemUtils.cs
+++ build-tools/TypeMake/Src/Base/FileSystemUtils.cs
@@ -16,17 +16,30 @@ namespace TypeMake
         public static IEnumerable<PathString> GetFiles(PathString Path, String SearchPattern, SearchOption SearchOption)
         {
             if (!Directory.Exists(Path)) { return new List<PathString>(); }
-            return Directory.EnumerateFiles(Path, SearchPattern, SearchOption).Select(f => f.AsPath());
+            return Directory.EnumerateFiles(Path, SearchPattern, SearchOption).OrderBy(f => f, StringComparer.Ordinal).Select(f => f.AsPath());
         }
         public static IEnumerable<PathString> GetDirectories(PathString Path, String SearchPattern, SearchOption SearchOption)
         {
             if (!Directory.Exists(Path)) { return new List<PathString>(); }
-            return Directory.EnumerateDirectories(Path, SearchPattern, SearchOption).Select(d => d.AsPath());
+            return Directory.EnumerateDirectories(Path, SearchPattern, SearchOption).OrderBy(f => f, StringComparer.Ordinal).Select(d => d.AsPath());
         }
         public static IEnumerable<PathString> GetFileSystemEntries(PathString Path, String SearchPattern, SearchOption SearchOption)
         {
-            if (!Directory.Exists(Path)) { return new List<PathString>(); }
-            return Directory.EnumerateFileSystemEntries(Path, SearchPattern, SearchOption).Select(f => f.AsPath());
+            if (!Directory.Exists(Path)) { yield break; }
+            foreach (var d in GetDirectories(Path, SearchPattern, SearchOption.TopDirectoryOnly))
+            {
+                if (SearchOption == SearchOption.AllDirectories)
+                {
+                    foreach (var f in GetFileSystemEntries(d, SearchPattern, SearchOption.TopDirectoryOnly))
+                    {
+                        yield return f;
+                    }
+                }
+            }
+            foreach (var f in GetFiles(Path, SearchPattern, SearchOption.TopDirectoryOnly))
+            {
+                yield return f;
+            }
         }
 
         public static IEnumerable<KeyValuePair<PathString, FileInfo>> GetRelativeFiles(PathString Path, String SearchPattern, SearchOption SearchOption)
